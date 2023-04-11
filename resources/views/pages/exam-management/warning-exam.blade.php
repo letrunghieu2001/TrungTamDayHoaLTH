17fa28abf7ca742ba058a406cf1b6261032aa45e
letrunghieu <hieult@regit-technology.com>
2023-04-11 16:39:37 +0700

    fix warning exam ui

--- resources/views/pages/exam-management/warning-exam.blade.php
+++ resources/views/pages/exam-management/warning-exam.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Thêm câu hỏi'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Lưu ý kiểm tra'])
     <div id="alert">
         @include('components.alert')
     </div>
