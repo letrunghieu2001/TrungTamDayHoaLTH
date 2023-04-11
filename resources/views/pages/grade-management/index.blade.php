40d39f209248f986711d5e88cfcde0921b7cb2ef
letrunghieu <hieult@regit-technology.com>
2023-04-11 16:03:16 +0700

    fix ui grade

--- resources/views/pages/grade-management/index.blade.php
+++ resources/views/pages/grade-management/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Quản lý người dùng'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Quản lý điểm'])
     <div class="row mt-4 mx-4">
         <div id="alert">
             @include('components.alert')
